bf9b2adede90ac1031dd03d89762edd4f452bae8
JordyV23 <jordyvargas0123@gmail.com>
2026-06-04 23:36:07 -0600

    Se pone en modo headless

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def main():
 
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
             page.goto(formulario_css)
